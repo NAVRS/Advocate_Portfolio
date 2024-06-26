4cf8d5fce4709cff2ee4ab42e55b02fc28f69371
NAVEEN RS <138767245+NAVRS@users.noreply.github.com>
2024-06-26 21:56:21 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+// portfolio.js
+
+// Smooth scrolling for links with hash
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
